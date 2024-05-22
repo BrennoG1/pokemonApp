83af5f88f1da3e4e37e3e3539165cb2cbf29b166
Brenno Grangeiro <brennoggrangeiro@hotmail.com>
2024-05-22 14:31:36 -0300

    Lista 5

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { PokeAPIService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,47 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  areaBuscarPokemon: string = '';
+  areaBusca: any = {
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf: ''
+  }
+
+  pokemon: any = {
+    name: '',
+    front_default: '',
+    abilities: '',
+    height: '',
+    weight: ''
+  }
+
+  constructor(
+    public photoService: PhotoService,
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: ViaCEPService ) {}
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  buscarPokemon() {
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon)
+      .subscribe((value) => {
+        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+        this.areaBusca.bairro     = ', ' + JSON.parse(JSON.stringify(value))['bairro'];
+        this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
+        this.areaBusca.uf         = '-' + JSON.parse(JSON.stringify(value))['uf'];
+      });
+    this.pokeAPIService.getPokeAPIService ()
+      .subscribe((value) => {
+        this.pokemon.name          = JSON.parse(JSON.stringify(value))['name'];
+        this.pokemon.front_default = JSON.parse(JSON.stringify(value))['sprites']['other']['dream_world']['front_default'];
+        this.pokemon.abilities     = JSON.parse(JSON.stringify(value))['abilities'].length;
+        this.pokemon.height        = JSON.parse(JSON.stringify(value))['height'];
+        this.pokemon.weight        = JSON.parse(JSON.stringify(value))['weight'];
+      });
+  }
 
 }
